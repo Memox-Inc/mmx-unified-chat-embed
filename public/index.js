2241185bf1b5c3507e7d0cdf1e87e18c03e94423
usama-690 <usama.nawaz@autonomoustech.ca>
2025-05-12 17:05:25 +0500

    fix: fixed chat embed config

---
+++ public/index.js
@@ -0,0 +1,20 @@
+class MyChatEmbed {
+    static init({ orgId, apiHost }) {
+        const iframe = document.createElement("iframe");
+        iframe.src = `${apiHost}/?orgId=${orgId}`;
+        iframe.style = `
+          position: fixed;
+          bottom: 20px;
+          right: 20px;
+          width: 350px;
+          height: 500px;
+          border: none;
+          z-index: 9999;
+        `;
+        document.body.appendChild(iframe);
+    }
+}
+
+// Make available both as ESM and global variable
+export default MyChatEmbed;
+if (typeof window !== "undefined") window.MyChatEmbed = MyChatEmbed;
