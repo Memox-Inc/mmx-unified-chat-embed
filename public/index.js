b14d1880fd567c98eec42808cf376d1b814d37ed
usama-690 <usama.nawaz@autonomoustech.ca>
2025-05-22 17:47:16 +0500

    updated web js files

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 class ChatBot {
-    static init({ orgId, apiHost }) {
-        const iframe = document.createElement("iframe");
-        iframe.src = `${apiHost}/?orgId=${orgId}`;
-        iframe.style = `
+  static init({ orgId }) {
+    const iframe = document.createElement("iframe");
+    const apiHost = "http://as84c4084sc4o8w8g80cs8wo.95.216.217.154.sslip.io/"
+    iframe.src = `${apiHost}/?orgId=${orgId}`;
+    iframe.style = `
           position: fixed;
           bottom: 20px;
           right: 20px;
@@ -11,8 +12,8 @@ class ChatBot {
           border: none;
           z-index: 9999;
         `;
-        document.body.appendChild(iframe);
-    }
+    document.body.appendChild(iframe);
+  }
 }
 
 // Make available both as ESM and global variable
